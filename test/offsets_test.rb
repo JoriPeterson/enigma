056eb9619c758e4a4f0e17179d4b1becc60948bc
Jori Peterson <joripeterson@gmail.com>
2019-06-01 16:23:20 -0600

    Update offsets object, start dreaming tests

--- test/offsets_test.rb
+++ test/offsets_test.rb
@@ -9,18 +9,27 @@ require 'pry'
 class OffsetsTest < Minitest::Test
 
   def setup
-    @offset = Offsets.new
     @key_1 = KeyGenerator.new
+
+    key = @key_1.generate_key
+    date = @key_1.generate_date
+
+    @offset = Offsets.new(key, date)
   end
 
   def test_it_exists
     assert_instance_of Offsets, @offset
   end
 
-  def test_it_creates_offsets
-    key = @key_1.generate_key
-    date = @key_1.generate_date
-
-    @offset.create_new_key(key, date)
+  def test_it_has_attributes
+    assert_equal 01234, @offset.key
+    assert_equal "060519", @offset.date
   end
+
+  # def test_it_creates_offsets
+  #   key = @key_1.generate_key
+  #   date = @key_1.generate_date
+  #
+  #   @offset.create_new_key(key, date)
+  # end
 end
