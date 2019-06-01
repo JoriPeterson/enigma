d7202587159a0396cb486c724bc0c0fce3e483f2
Jori Peterson <joripeterson@gmail.com>
2019-06-01 14:57:14 -0600

    Add testing outline for key_gen class

--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -8,8 +8,20 @@ require 'pry'
 
 class KeyGeneratorTest < Minitest::Test
 
+  def setup
+    @key_gen = KeyGenerator.new
+  end
+
   def test_it_exists
-    key_gen = KeyGenerator.new
-    assert_instance_of KeyGenerator, key_gen
+    assert_instance_of KeyGenerator, @key_gen
+  end
+
+  def test_it_generate_5_digit_key
+    assert_equal 5, @key_gen.generate_key.size
+  end
+
+  def test_it_generates_a_date_in_ddmmyy_format
+    skip
+    #use a stub here?
   end
 end
