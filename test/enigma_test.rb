a9e61bebed7871b7b58b221347bc342afce241c2
Jori Peterson <joripeterson@gmail.com>
2019-06-01 14:41:41 -0600

    Create first set of encrypt and decrypt tests

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -6,8 +6,31 @@ require 'pry'
 
 class EnigmaTest < Minitest::Test
 
+  def setup
+    @enigma = Enigma.new
+  end
+
   def test_it_exists
-    enigma = Enigma.new
-    assert_instance_of Enigma, enigma
+    assert_instance_of Enigma, @enigma
+  end
+
+  def test_it_encrypts_messages_with_a_key_and_a_date
+    expected =  {
+    encryption: "keder ohulw",
+    key: "02715",
+    date: "040895" }
+    actual = @enigma.encrypt("hello world", "02715", "040895")
+
+    assert_equal expected, actual
+  end
+
+  def test_it_decrypts_messages_with_a_key_and_a_date
+    expected =  {
+    decryption: "hello world",
+    key: "02715",
+    date: "040895"  }
+    actual = @enigma.decrypt("keder ohulw", "02715", "040895")
+
+    assert_equal expected, actual
   end
 end
