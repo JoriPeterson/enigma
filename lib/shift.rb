07c69f745f3ba532e03dfca90c67236c7e557b24
Jori Peterson <joripeterson@gmail.com>
2019-06-01 19:07:16 -0600

    Commit ideas for shift class

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,4 +1,13 @@
 class Shift
+  attr_reader :message, :offsets
 
+  def initialize(message, offsets = Offsets.new.create_offsets)
+    @message = message
+    @offsets = offsets
+  end
+
+  # Shift is responsible for encryption and decryption
+  # based on the shift of the keys A-D
+  # Each key has to be rotated by the correlating offset
 
 end
