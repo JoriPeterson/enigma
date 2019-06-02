7f32511733e8d9b89a23ea5030bce57c50bfb5eb
Jori Peterson <joripeterson@gmail.com>
2019-06-02 15:16:52 -0600

    Creating encrypt method with class integration

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,13 @@
 class Enigma
 
+  def encrypt(message, key = nil, date = nil)
+    offsets = Offsets.new(key, date)
+    shift = Shift.new(offsets.create_offsets)
+
+    { encryption: shift.encrypt(message),
+      key: offsets.key,
+      date: offsets.date
+    }
+  end
 
 end
