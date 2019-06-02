1d821142816342da2194432f552e17a43aba766c
Jori Peterson <joripeterson@gmail.com>
2019-06-02 15:14:10 -0600

    Remove message from initialize

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,8 +1,7 @@
 class Shift
-  attr_reader :message, :offsets
+  attr_reader :offsets
 
-  def initialize(message, offsets = Offsets.new.create_offsets)
-    @message = message
+  def initialize(offsets = Offsets.new.create_offsets)
     @offsets = offsets
   end
 
@@ -19,13 +18,13 @@ class Shift
     new_message = []
     array.each_with_index do |char, message_index|
       index = find_index_of_character(char)
-        if message_index % 3 == 0
+        if message_index % 4 == 0
           new_message << character_map.rotate(@offsets[0])[index]
-        elsif message_index % 3 == 1
+        elsif message_index % 4 == 1
           new_message << character_map.rotate(@offsets[1])[index]
-        elsif message_index % 3 == 2
+        elsif message_index % 4 == 2
           new_message << character_map.rotate(@offsets[2])[index]
-        elsif message_index % 3 == 3
+        elsif message_index % 4 == 3
           new_message << character_map.rotate(@offsets[3])[index]
         end
     end
@@ -37,13 +36,13 @@ class Shift
     new_message = []
     array.each_with_index do |char, message_index|
       index = find_index_of_character(char)
-        if message_index % 3 == 0
+        if message_index % 4 == 0
           new_message << character_map.rotate(-@offsets[0])[index]
-        elsif message_index % 3 == 1
+        elsif message_index % 4 == 1
           new_message << character_map.rotate(-@offsets[1])[index]
-        elsif message_index % 3 == 2
+        elsif message_index % 4 == 2
           new_message << character_map.rotate(-@offsets[2])[index]
-        elsif message_index % 3 == 3
+        elsif message_index % 4 == 3
           new_message << character_map.rotate(-@offsets[3])[index]
         end
     end
