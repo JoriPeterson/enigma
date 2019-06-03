d50ca58b93f0b52401bd67d2ebfe32d2000bd803
Jori Peterson <joripeterson@gmail.com>
2019-06-02 21:25:15 -0600

    Switch to string interpolation for encryption text

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -9,4 +9,4 @@ hash = enigma.encrypt(message)
 file = File.open(ARGV[1], 'w')
 file.puts hash[:encryption]
 
-p "Created 'encrypted.txt' with the key #{hash[:key]} and date #{hash[:date]}"
+p "Created '#{ARGV[1]}' with the key #{hash[:key]} and date #{hash[:date]}"
