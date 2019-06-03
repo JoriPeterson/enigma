b5c445cdb1ba8236eb0b1fb1a8e5202ce9f082b2
Jori Peterson <joripeterson@gmail.com>
2019-06-02 22:05:11 -0600

    Refactor encrypt runner file

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -3,10 +3,13 @@ require './lib/key_generator'
 require './lib/offsets'
 require './lib/shift'
 
+message_txt = ARGV[0]
+encrypted = ARGV[1]
+
 enigma = Enigma.new
-message = File.read(ARGV[0]).chomp.downcase
+message = File.read(message_txt).chomp.downcase
 hash = enigma.encrypt(message)
-file = File.open(ARGV[1], 'w')
+file = File.open(encrypted, 'w')
 file.puts hash[:encryption]
 
-p "Created '#{ARGV[1]}' with the key #{hash[:key]} and date #{hash[:date]}"
+p "Created '#{encrypted}' with the key #{hash[:key]} and date #{hash[:date]}"
