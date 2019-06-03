e720d6bf1ab00895349beb84e9abcf462e3c9ab7
Jori Peterson <joripeterson@gmail.com>
2019-06-02 22:00:44 -0600

    Refactor decrypt runner file

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -3,10 +3,15 @@ require './lib/key_generator'
 require './lib/offsets'
 require './lib/shift'
 
+encryption = ARGV[0]
+decryption = ARGV[1]
+key = ARGV[2]
+date = ARGV[3]
+
 enigma = Enigma.new
-message = File.read(ARGV[0])
-hash = enigma.decrypt(message, ARGV[2], ARGV[3])
-file = File.open(ARGV[1], 'w')
+encrypt_file = File.read(encryption)
+file = File.open(decryption, 'w')
+hash = enigma.decrypt(encrypt_file, key, date)
 file.puts hash[:decryption]
 
-p "Created '#{ARGV[1]}' with the key #{hash[:key]} and date #{hash[:date]}"
+p "Created '#{decryption}' with the key #{hash[:key]} and date #{hash[:date]}"
