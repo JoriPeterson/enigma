f2270736cbec0b681fd8a3d145941f4e27bb8b08
Jori Peterson <joripeterson@gmail.com>
2019-06-03 09:25:03 -0600

    Refactor minor changes on decrypt runner and key_gen

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -3,14 +3,14 @@ require './lib/key_generator'
 require './lib/offsets'
 require './lib/shift'
 
-encryption = ARGV[0]
-decryption = ARGV[1]
+encrypted = ARGV[0]
+decrypted = ARGV[1]
 key = ARGV[2]
 date = ARGV[3]
 
 enigma = Enigma.new
-encrypt_file = File.read(encryption)
-file = File.open(decryption, 'w')
+encrypt_file = File.read(encrypted)
+file = File.open(decrypted, 'w')
 hash = enigma.decrypt(encrypt_file, key, date)
 file.puts hash[:decryption]
 
--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -3,11 +3,10 @@ require 'Date'
 class KeyGenerator
 
   def generate_key
-    5.times.map{ rand(0..9)}.join
+    5.times.map{rand(0..9) }.join
   end
 
   def generate_date
-    date = Date.today
-    date.strftime("%d%m%y")
+    Date.today.strftime("%d%m%y")
   end
 end
